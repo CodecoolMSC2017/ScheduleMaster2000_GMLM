8863c47226d21446137fe65b0f4e609ffbb15559
Bernáth Máté <bernath.mate.new@gmail.com>
2018-05-14 11:39:15 +0200

    onProfileLoad() method is implemented, this method will show the menu content, after the successful login

---
+++ src/main/webapp/profile.js
@@ -0,0 +1,7 @@
+function onProfileLoad(user) {
+    clearMessages();
+    showContents(['menu-content']);
+
+    const userNameSpanEl = document.getElementById('user-name');
+    userNameSpanEl.textContent = user.name;
+}
