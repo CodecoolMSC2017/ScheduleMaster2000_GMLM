849da24b7b4cafbe032649a65c2dd1b272047cd5
Bernáth Máté <bernath.mate.new@gmail.com>
2018-05-14 14:52:52 +0200

    clearForm is added, this method will clear all input field in the given form element

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -6,6 +6,7 @@ const INTERNAL_SERVER_ERROR = 500;
 
 let loginContentDivEl;
 let registerContentDivEl;
+let registerFormEl;
 let menuContentDivEl;
 
 function newInfo(targetEl, message) {
@@ -25,6 +26,10 @@ function newMessage(targetEl, cssClass, message) {
     pEl.textContent = message;
 
     targetEl.appendChild(pEl);
+}
+
+function clearForm(form) {
+    form.reset();
 }
 
 function clearMessages() {
@@ -95,6 +100,7 @@ function setUnauthorized() {
 function onLoad() {
     loginContentDivEl = document.getElementById('login-content');
     registerContentDivEl = document.getElementById('register-content');
+    registerFormEl = document.getElementById('register-form');
     menuContentDivEl = document.getElementById('menu-content');
 
     const loginButtonEl = document.getElementById('login-button');
