94fbeeba40256839b5ef32eb468b4502dd2e6e0f
GaborMartin <gabormartin94@gmail.com>
2018-05-15 14:26:48 +0200

    Implemented basic task.js

---
+++ src/main/webapp/task.js
@@ -0,0 +1,30 @@
+function onTaskLoad(task) {
+    const taskContentDivEl = document.getElementById('task-content');
+
+    const nameHEl = document.createElement('h3');
+    nameHEl.textContent = "Name:";
+
+    const nameSpanEl = document.createElement('span');
+    nameSpanEl.textContent = task.name;
+
+    const contentHEl = document.createElement('h3');
+    contentHEl.textContent = "Content:";
+
+    const contentSpanEl = document.createElement('span');
+    contentSpanEl.textContent = task.content;
+
+    taskContentDivEl.append(nameHEl);
+    taskContentDivEl.append(nameSpanEl);
+    taskContentDivEl.append(contentHEl);
+    taskContentDivEl.append(contentSpanEl);
+}
+
+function onTaskResponse() {
+    if (this.status === OK) {
+        clearMessages();
+        showContents(['menu-content', 'task-content']);
+        onTaskLoad(JSON.parse(this.responseText));
+    } else {
+        onOtherResponse(tasksContentDivEl, this);
+    }
+}
