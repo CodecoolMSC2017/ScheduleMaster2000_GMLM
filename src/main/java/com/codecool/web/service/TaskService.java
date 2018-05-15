c1e3ed37a4a8eacff11e3d7709dd3f9827118037
Kontra László <kontravlaszlo@gmail.com>
2018-05-15 11:46:55 +0200

    SimpleTaskService and TaskService interface implemented without the previously mentioned missing get task by day_id and delete task functions

---
+++ src/main/java/com/codecool/web/service/TaskService.java
@@ -0,0 +1,16 @@
+package com.codecool.web.service;
+
+import com.codecool.web.model.Task;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface TaskService {
+
+    Task addNewTask(String name, String content, int user_id) throws SQLException, ServiceException;
+    void updateTask(String name, String content, int task_id) throws SQLException, ServiceException;
+    Task findById(int id) throws SQLException, ServiceException;
+    List<Task> getAllTasks() throws SQLException, ServiceException;
+    List<Task> getUsersTask(int user_id) throws SQLException, ServiceException;
+}
---
+++ src/main/java/com/codecool/web/service/simple/SimpleTaskService.java
@@ -0,0 +1,62 @@
+package com.codecool.web.service.simple;
+
+import com.codecool.web.dao.TaskDao;
+import com.codecool.web.model.Task;
+import com.codecool.web.service.TaskService;
+import com.codecool.web.service.exception.ServiceException;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class SimpleTaskService implements TaskService {
+    private final TaskDao taskDao;
+
+    public SimpleTaskService(TaskDao taskDao) {
+        this.taskDao = taskDao;
+    }
+
+    @Override
+    public Task addNewTask(String name, String content, int user_id) throws SQLException, ServiceException {
+        try {
+            return taskDao.addNewTask(name, content, user_id);
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void updateTask(String name, String content, int task_id) throws SQLException, ServiceException {
+        try {
+            taskDao.updateTask(name, content, task_id);
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public Task findById(int id) throws SQLException, ServiceException {
+        try {
+            return taskDao.findById(id);
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public List<Task> getAllTasks() throws SQLException, ServiceException {
+        try {
+            return taskDao.getAllTasks();
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public List<Task> getUsersTask(int user_id) throws SQLException, ServiceException {
+        try {
+            return taskDao.getUsersTask(user_id);
+        } catch (IllegalArgumentException ex) {
+            throw new ServiceException(ex.getMessage());
+        }
+    }
+}
