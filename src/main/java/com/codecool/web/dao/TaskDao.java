7743178938215535daf8c8cdce0dab084b947596
Kontra László <kontravlaszlo@gmail.com>
2018-05-15 11:35:29 +0200

    TaskDao interface and DatabaseTaskDao is partially done: still need deleting task, and finding task by day_id

---
+++ src/main/java/com/codecool/web/dao/TaskDao.java
@@ -0,0 +1,15 @@
+package com.codecool.web.dao;
+
+import com.codecool.web.model.Task;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface TaskDao {
+
+    Task addNewTask(String name, String content, int user_id) throws SQLException;
+    void updateTask(String name, String content, int task_id) throws SQLException;
+    Task findById(int id) throws SQLException;
+    List<Task> getAllTasks() throws SQLException;
+    List<Task> getUsersTask(int user_id) throws SQLException;
+}
---
+++ src/main/java/com/codecool/web/dao/database/DatabaseTaskDao.java
@@ -0,0 +1,125 @@
+package com.codecool.web.dao.database;
+
+import com.codecool.web.dao.TaskDao;
+import com.codecool.web.model.Task;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseTaskDao extends AbstractDao implements TaskDao {
+
+    public DatabaseTaskDao(Connection connection) {
+        super(connection);
+    }
+
+    public Task addNewTask(String name, String content, int user_id) throws SQLException {
+        if (name == null || name.equals("")) {
+            throw new IllegalArgumentException("Name can not be empty");
+        }
+        if (!doesTaskExists(name)) {
+            throw new IllegalArgumentException("Task with this name already exists");
+        }
+        boolean autoCommit = connection.getAutoCommit();
+        connection.setAutoCommit(false);
+        String sql = "INSERT INTO tasks (name,content,user_id) VALUES (?, ?, ?);";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, content);
+            preparedStatement.setInt(3, user_id);
+            executeInsert(preparedStatement);
+            int id = fetchGeneratedId(preparedStatement);
+            return new Task(id, name, content);
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        } finally {
+            connection.setAutoCommit(autoCommit);
+        }
+    }
+
+    public void updateTask(String name, String content, int task_id) throws SQLException {
+        if (doesTaskExists(name)) {
+            throw new IllegalArgumentException("A task with that name already exists");
+        }
+        boolean autoCommit = connection.getAutoCommit();
+        connection.setAutoCommit(false);
+        String sql = "UPDATE tasks SET name = ?, content = ? WHERE id = ?;";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, content);
+            preparedStatement.setInt(3, task_id);
+            executeInsert(preparedStatement);
+            connection.commit();
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        } finally {
+            connection.setAutoCommit(autoCommit);
+        }
+    }
+
+    private boolean doesTaskExists(String name) throws SQLException {
+        if (name == null || name.equals("")) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        String sql = "SELECT * FROM tasks WHERE name = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, name);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (!resultSet.next()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public Task findById(int id) throws SQLException {
+        String sql = "SELECT * FROM tasks WHERE id = ?;";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return fetchTask(resultSet);
+                }
+            }
+        }
+        return null;
+    }
+
+    public List<Task> getAllTasks() throws SQLException {
+        String sql = "SELECT * FROM tasks;";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery(sql)) {
+            List<Task> allTasks = new ArrayList<>();
+            while (resultSet.next()) {
+                allTasks.add(fetchTask(resultSet));
+            }
+            return allTasks;
+        }
+    }
+
+    public List<Task> getUsersTask(int user_id) throws SQLException {
+        String sql = "SELECT * FROM tasks WHERE user_id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            List<Task> usersTask = new ArrayList<>();
+            preparedStatement.setInt(1, user_id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    usersTask.add(fetchTask(resultSet));
+                }
+            }
+            return usersTask;
+        }
+    }
+
+
+    private Task fetchTask(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("id");
+        String name = resultSet.getString("name");
+        String content = resultSet.getString("content");
+        return new Task(id, name, content);
+
+    }
+}
