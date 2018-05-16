2e72fff3397b4a843221863952864248a6a7247d
GaborMartin <gabormartin94@gmail.com>
2018-05-16 16:34:41 +0200

    Changed DAO and Service, changed used method from service to getTaskByDayId

--- src/main/java/com/codecool/web/servlet/DayServlet.java
+++ src/main/java/com/codecool/web/servlet/DayServlet.java
@@ -1,11 +1,12 @@
 package com.codecool.web.servlet;
 
-import com.codecool.web.dao.DayDao;
-import com.codecool.web.dao.database.DatabaseDayDao;
-import com.codecool.web.model.Day;
-import com.codecool.web.service.DayService;
+
+import com.codecool.web.dao.TaskDao;
+import com.codecool.web.dao.database.DatabaseTaskDao;
+import com.codecool.web.model.Task;
+import com.codecool.web.service.TaskService;
 import com.codecool.web.service.exception.ServiceException;
-import com.codecool.web.service.simple.SimpleDayService;
+import com.codecool.web.service.simple.SimpleTaskService;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/protected/day")
 public class DayServlet extends AbstractServlet {
@@ -20,13 +22,14 @@ public class DayServlet extends AbstractServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         try (Connection connection = getConnection(req.getServletContext())) {
-            DayDao dayDao = new DatabaseDayDao(connection);
-            DayService dayService = new SimpleDayService(dayDao);
+            TaskDao taskDao = new DatabaseTaskDao(connection);
+            TaskService taskService = new SimpleTaskService(taskDao);
 
             String dayId = req.getParameter("id");
 
-            Day day = dayService.findById(Integer.parseInt(dayId));
-            sendMessage(resp, 200, day);
+            List<Task> tasks = taskService.getTaskByDayId(Integer.parseInt(dayId));
+
+            sendMessage(resp, 200, tasks);
         } catch (SQLException ex) {
             handleSqlError(resp, ex);
         } catch (ServiceException e) {
