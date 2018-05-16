814acc553e5be30c2184952654883e91af961af1
GaborMartin <gabormartin94@gmail.com>
2018-05-16 16:33:24 +0200

    Added hour field to Task model; Added new constructor, getter.

--- src/main/java/com/codecool/web/model/Task.java
+++ src/main/java/com/codecool/web/model/Task.java
@@ -3,6 +3,14 @@ package com.codecool.web.model;
 public class Task extends AbstractModel {
     private String name;
     private String content;
+    private int hour;
+
+    public Task(int id, String name, String content, int hour) {
+        super(id);
+        this.name = name;
+        this.content = content;
+        this.hour = hour;
+    }
 
     public Task(int id, String name, String content) {
         super(id);
@@ -18,4 +26,7 @@ public class Task extends AbstractModel {
         return content;
     }
 
+    public int getHour() {
+        return hour;
+    }
 }
