a255694807909f502849035284cb4313eb36506f
Kontra László <kontravlaszlo@gmail.com>
2018-05-16 15:14:09 +0200

    Rewrote ScheduleServlet so it now sends days

--- src/main/java/com/codecool/web/servlet/ScheduleServlet.java
+++ src/main/java/com/codecool/web/servlet/ScheduleServlet.java
@@ -1,34 +1,34 @@
 package com.codecool.web.servlet;
 
 
-import com.codecool.web.dao.ScheduleDao;
-import com.codecool.web.dao.database.DatabaseScheduleDao;
-import com.codecool.web.model.Schedule;
-import com.codecool.web.service.ScheduleService;
+import com.codecool.web.dao.DayDao;
+import com.codecool.web.dao.database.DatabaseDayDao;
+import com.codecool.web.model.Day;
+import com.codecool.web.service.DayService;
 
 import com.codecool.web.service.exception.ServiceException;
-import com.codecool.web.service.simple.SimpleScheduleService;
+import com.codecool.web.service.simple.SimpleDayService;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/protected/schedule")
 public class ScheduleServlet extends AbstractServlet {
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         try (Connection connection = getConnection(req.getServletContext())) {
-            ScheduleDao scheduleDao = new DatabaseScheduleDao(connection);
-            ScheduleService scheduleService = new SimpleScheduleService(scheduleDao);
+            DayDao dayDao = new DatabaseDayDao(connection);
+            DayService dayService = new SimpleDayService(dayDao);
 
             String scheduleId = req.getParameter("id");
 
-            Schedule schedule = scheduleService.getScheduleById(Integer.parseInt(scheduleId));
-            sendMessage(resp, 200, schedule);
+            List<Day> days = dayService.findDaysByScheduleId(Integer.parseInt(scheduleId));
+            sendMessage(resp, 200, days);
         } catch (ServiceException e) {
             sendMessage(resp, 401, e.getMessage());
         } catch (SQLException e) {
