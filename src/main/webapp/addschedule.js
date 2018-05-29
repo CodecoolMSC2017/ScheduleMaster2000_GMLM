3f73db7857de134cdec04cee05de2fd9631dfa27
Kontra László <kontravlaszlo@gmail.com>
2018-05-29 15:10:05 +0200

    renamed function names, so will not get confuse them by other file's functions

--- src/main/webapp/addschedule.js
+++ src/main/webapp/addschedule.js
@@ -5,7 +5,7 @@ function onAddScheduleResponse() {
     removeAllChildren(scheduleInfoContentDivId);
     if(this.status === OK) {
         addScheduleForm.reset();
-        onCloseSpanClicked();
+        onScheduleCloseSpanClicked();
         onSchedulesButtonClicked();
     }
     const pEl = document.createElement('p');
@@ -14,7 +14,7 @@ function onAddScheduleResponse() {
     scheduleInfoContentDivId.append(pEl);
 }
 
-function onSaveButtonClicked() {
+function onSaveScheduleButtonClicked() {
     const scheduleNameInputEl = document.getElementById('new-schedule-name');
 
     scheduleName = scheduleNameInputEl.value;
@@ -29,7 +29,7 @@ function onSaveButtonClicked() {
     xhr.send(params);
 }
 
-function onCloseSpanClicked() {
+function onScheduleCloseSpanClicked() {
     popupScheduleFormDivEl.style.display = "none";
 }
 
@@ -37,12 +37,12 @@ function onAddScheduleButtonClicked() {
     addScheduleForm = document.getElementById('add-schedule-form');
     addScheduleForm.reset();
     const addScheduleButtonEl = document.getElementById('save-schedule-button');
-    addScheduleButtonEl.addEventListener('click', onSaveButtonClicked);
+    addScheduleButtonEl.addEventListener('click', onSaveScheduleButtonClicked);
 
     popupScheduleFormDivEl = document.getElementById('add-schedule-modal');
 
     const closeSpan = document.getElementById('close-schedule-modal');
-    closeSpan.addEventListener('click', onCloseSpanClicked);
+    closeSpan.addEventListener('click', onScheduleCloseSpanClicked);
 
     removeAllChildren(infoContentDivEl);
     popupScheduleFormDivEl.style.display = "block";
