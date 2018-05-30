cb05088d6be597c3ada6ae955b49c8113351aca6
GaborMartin <gabormartin94@gmail.com>
2018-05-30 14:27:51 +0200

    Added getAllUsers method to interface, implemented method in SimpleUserService

--- src/main/java/com/codecool/web/service/UserService.java
+++ src/main/java/com/codecool/web/service/UserService.java
@@ -4,10 +4,11 @@ import com.codecool.web.model.User;
 import com.codecool.web.service.exception.ServiceException;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface UserService {
 
     User findUserByEmail(String email) throws SQLException, ServiceException;
     User addNewUser(String name, String email, String password) throws SQLException, IllegalArgumentException, ServiceException;
-
+    List<User> getAllUsers() throws SQLException, ServiceException;
 }
