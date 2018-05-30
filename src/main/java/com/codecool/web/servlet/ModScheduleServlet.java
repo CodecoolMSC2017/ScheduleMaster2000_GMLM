219948f181cd311bc9f9dd8881aec3cc7e80eb7a
Gergely Murvai <murvai.gergely@gmail.com>
2018-05-30 16:43:01 +0200

    New servlet to get one schedule

---
+++ src/main/java/com/codecool/web/servlet/ModScheduleServlet.java
@@ -0,0 +1,39 @@
+package com.codecool.web.servlet;
+
+import com.codecool.web.dao.ScheduleDao;
+import com.codecool.web.dao.database.DatabaseScheduleDao;
+import com.codecool.web.model.Schedule;
+
+import com.codecool.web.service.ScheduleService;
+import com.codecool.web.service.exception.ServiceException;
+import com.codecool.web.service.simple.SimpleScheduleService;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+@WebServlet ("/protected/modschedule")
+public class ModScheduleServlet extends AbstractServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try (Connection connection = getConnection(req.getServletContext())) {
+            ScheduleDao scheduleDao = new DatabaseScheduleDao(connection);
+            ScheduleService scheduleService = new SimpleScheduleService(scheduleDao);
+
+            String scheduleId = req.getParameter("id");
+
+            Schedule schedule = scheduleService.getScheduleById(Integer.parseInt(scheduleId));
+            sendMessage(resp, 200, schedule);
+        } catch (ServiceException e) {
+            sendMessage(resp, 401, e.getMessage());
+        } catch (SQLException e) {
+            handleSqlError(resp, e);
+        }
+    }
+
+}
