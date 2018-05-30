e8802ab21ba43d2ec2233a758080c7b73b8faf7b
Gergely Murvai <murvai.gergely@gmail.com>
2018-05-30 12:04:14 +0200

    Added new method to send proper GET request to protected/schedule

--- src/main/webapp/addDay.js
+++ src/main/webapp/addDay.js
@@ -1,12 +1,24 @@
 let popupAddDayFormDivEl
 let addDayForm;
+let scheduleId;
+
+function getModifiedScheduleAfterDelete() {
+    const params = new URLSearchParams();
+    params.append('id', scheduleId);
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onScheduleResponse);
+    xhr.addEventListener('error', onNetworkError);
+    xhr.open('GET', 'protected/schedule?' + params.toString());
+    xhr.send(params);
+}
 
 function onAddDayResponse() {
     removeAllChildren(infoContentDivEl);
     if(this.status === OK) {
         addDayForm.reset();
         onCloseSpanClicked();
-        onScheduleClicked();
+        getModifiedScheduleAfterDelete();
     }
     const pEl = document.createElement('p');
     const response = JSON.parse(this.responseText);
@@ -16,10 +28,11 @@ function onAddDayResponse() {
 
 function onSaveDayButtonClicked() {
     const dayTitleInputEl = document.getElementById('new-day-title');
-
     dayTitle = dayTitleInputEl.value;
 
+
     const params = new URLSearchParams();
+    params.append('id',scheduleId);
     params.append('title', dayTitle);
 
     const xhr = new XMLHttpRequest();
@@ -31,10 +44,13 @@ function onSaveDayButtonClicked() {
 }
 
 function onCloseSpanClicked() {
-    popupFormDivEl.style.display = "none";
+    popupAddDayFormDivEl.style.display = "none";
 }
 
 function onAddDayButtonClicked() {
+    const addNewDayButtonEl = document.getElementById("add-new-day");
+
+    scheduleId = addNewDayButtonEl.dataset.scheduleId;
     addDayForm = document.getElementById('add-day-form');
     addDayForm.reset();
     const addDayButtonEl = document.getElementById('save-day-button');
@@ -46,6 +62,6 @@ function onAddDayButtonClicked() {
     closeSpan.addEventListener('click', onCloseSpanClicked);
 
     removeAllChildren(infoContentDivEl);
-    popupFormDivEl.style.display = "block";
-    showContents(['save-day-button', 'schedule-content', 'menu-content', 'add-day-button-clicked']);
+    popupAddDayFormDivEl.style.display = "block";
+    showContents(['save-day-button', 'schedule-content', 'menu-content', 'add-day-button-content']);
 }
